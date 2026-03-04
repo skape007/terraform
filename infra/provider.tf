07d70799faebec5c0ae55105ac5a339e74f4e84f
skape007 <antonio127@gmail.com>
2026-03-04 23:18:46 +0000

    enhance: add default tags for AWS provider in provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,12 @@
 provider "aws" {
   region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = "azure-automations"
+      Environment = var.stack_name
+      ManagedBy   = "terraform"
+    }
+  }
 }
 
